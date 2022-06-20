99536c167038f99e67bbadcae35aeadb6c940ddb
squaredS <singh.ss.sudeep@gmail.com>
2022-06-20 13:24:04 +0530

    Updated print statement

--- DataStructures/src/topic01_recursion/Tut01_print.java
+++ DataStructures/src/topic01_recursion/Tut01_print.java
@@ -3,23 +3,23 @@ package topic01_recursion;
 import java.util.Scanner;
 
 public class Tut01_print {
-	
+
 	public static void fun(int count, int n) {
-		if(count== n) {
+		if (count == n) {
 			return;
 		}
-		
-		System.out.println(count);
+
+		System.out.println("Count = " + count);
 		count++;
 		fun(count, n);
 	}
-	
+
 	public static void main(String[] args) {
-		
-		Scanner sc= new Scanner(System.in);
-		int n= sc.nextInt();
-		
-		int count= 0;
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int count = 0;
 		fun(count, n);
 		sc.close();
 	}
